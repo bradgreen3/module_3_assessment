0cc9209be0d2064611278ac0a18d31c102871000
Brad <bgreen3@uoregon.edu>
2017-01-06 12:10:12 -0700

    Begin setup for final test

---
+++ spec/features/stores/user_sees_store_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "user clicks on store" do
+  xit "sees info about that store" do
+    VCR.use_cassette("#showstore") do
+
+    visit '/'
+
+    fill_in "zip", with: "80202"
+
+    click_on "Search"
+
+    # When I click the name of a store
+
+    expect(current_path).to eq(stores_path(store.id))
+
+    # I should see the store name, store type and address with city, state and zip
+    # I should see an unordered list of the store hours in the following format:
+
+  end
+end
