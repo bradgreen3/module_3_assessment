2e33e4010f82308a67ccd2124509daf27972365e
Brad <bgreen3@uoregon.edu>
2017-01-06 10:27:15 -0700

    Set up first feature test for consuming API

---
+++ spec/features/bestbuy_search_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "a user who searches for a store" do
+  it "sees appropriate information returned" do
+
+  visit '/'
+
+  fill_in "Zip", with: "80202"
+
+  click_on "Search"
+
+  expect(current_path).to eq('/search')
+  expect(page).to have_content('all the stores within 25 miles')
+  expect(page).to have_content('message saying: 15 total stores')
+  expect(page).to have_content('there to be exactly 15 stores on the page')
+  expect(page).to have_content('long name, city, distance, phone number and store type for each of the 15 results')
+  end
+end
