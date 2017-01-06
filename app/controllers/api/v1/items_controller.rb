20194b278412fee4ebc6374d648517efd6b00bc8
Brad <bgreen3@uoregon.edu>
2017-01-06 10:10:58 -0700

    Kind of stuck with strong params issue

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,8 +8,23 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.find(params[:id])
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render json: Item.create(item_params)
+    else
+      render status: 400
+    end
+  end
+
   def destroy
     render json: Item.delete(params[:id]), status: 204
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
   end
 
 end
